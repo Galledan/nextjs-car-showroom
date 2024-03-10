ca7dfe9621a59932e65a90caf122a8974b8c91db
Galledan <onrclklr@outlook.com>
2024-03-10 15:38:52 +0300

    Added Search Bar with Filters

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,11 @@
 import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
 import Footer from "./Footer";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
 
-export { Hero, CustomButton, Navbar, Footer };
+
+
+export { Hero, CustomButton,CustomFilter,SearchBar,SearchManufacturer, Navbar, Footer };
