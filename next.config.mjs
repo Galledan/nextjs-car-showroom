32e3717fe91f9f97d76ff4710ecd70cff0a7f2f9
Galledan <onrclklr@outlook.com>
2024-03-10 21:51:27 +0300

    Changes for first deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['dn.imagin.studio']
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    }
+};
 
 export default nextConfig;
